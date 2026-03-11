0a09e8920dd99ff5a5772303d3f27a6c5fac106d
ANALICE VITAL PEREIRA <analice.pereira@portalsesisp.org.br>
2026-03-11 09:02:16 -0300

    Commit inicial: EStrutura do projeto

---
+++ escopo.js
@@ -0,0 +1,11 @@
+function exemplo() {
+    let numero = 10;
+
+    if (numero > 5) {
+        let mensagem = "0 númeor é maior que 5. ";
+        console.log(mensagem); // Acessivel dentro do bloco if 
+    }
+    console.log(numero);
+}
+exemplo();
+    
